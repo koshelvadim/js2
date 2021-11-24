783fe24cac0800d5983b1ab4e8076b20396a5c54
koshelvadim <koshelvadim@mail.ru>
2021-11-24 12:45:28 +0300

    less7

--- js/ErrorComponent.js
+++
@@ -1,9 +0,0 @@
-Vue.component('error', {
-    props: ['visibility'],
-   
-    template:   `
-                <div class="error" v-show="visibility">
-                    <h1>SERVER ERROR</h1>
-                </div>
-                `
-});
